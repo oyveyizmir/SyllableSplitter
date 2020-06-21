d93c42665d488f6e2ed489376997ec398d268848
Vyacheslav Tkachuk <vyacheslav.tkachuk@gmail.com>
2020-06-21 14:32:26 +0300

    More tests

--- SyllableSplitterUnitTests/Ukrainian2Test.cs
+++ SyllableSplitterUnitTests/Ukrainian2Test.cs
@@ -14,7 +14,35 @@ namespace SyllableSplitterUnitTests
         protected override string RuleFileName => "ukrainian2.json";
 
         [Test]
-        [TestCaseSource("GetTestData", new object[] { "Ukrainian2.csv" })]
-        public void TestBreakWord(string word, string expected) => BreakWord(word, expected);
+        [TestCaseSource("GetTestData", new object[] { "Ukrainian2-гл-гл.csv" })]
+        public void TestГлухийГлухий(string word, string expected) => BreakWord(word, expected);
+
+        [Test]
+        [TestCaseSource("GetTestData", new object[] { "Ukrainian2-гл-сон.csv" })]
+        public void TestГлухийСонорний(string word, string expected) => BreakWord(word, expected);
+
+        [Test]
+        [TestCaseSource("GetTestData", new object[] { "Ukrainian2-дз-сон.csv" })]
+        public void TestДзвінкийСонорний(string word, string expected) => BreakWord(word, expected);
+
+        [Test]
+        [TestCaseSource("GetTestData", new object[] { "Ukrainian2-сон-гл.csv" })]
+        public void TestСонорнийГлухий(string word, string expected) => BreakWord(word, expected);
+
+        [Test]
+        [TestCaseSource("GetTestData", new object[] { "Ukrainian2-сон-сон.csv" })]
+        public void TestСонорнийСонорний(string word, string expected) => BreakWord(word, expected);
+
+        [Test]
+        [TestCaseSource("GetTestData", new object[] { "Ukrainian2-гл-гл-сон.csv" })]
+        public void TestГлухийГлухийСонорний(string word, string expected) => BreakWord(word, expected);
+
+        [Test]
+        [TestCaseSource("GetTestData", new object[] { "Ukrainian2-дз-дз.csv" })]
+        public void TestДзвінкийДзвінкий(string word, string expected) => BreakWord(word, expected);
+
+        [Test]
+        [TestCaseSource("GetTestData", new object[] { "Ukrainian2-дз-гл.csv" })]
+        public void TestДзвінкийГлухий(string word, string expected) => BreakWord(word, expected);
     }
 }
